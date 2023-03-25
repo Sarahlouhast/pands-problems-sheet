60e53073d0a5fa9a650f2453396c8503d97a9114
Sarah Hastings <G00235562@atu.ie>
2023-03-25 20:13:25 +0000

    Topic02task

---
+++ bank.py
@@ -0,0 +1,15 @@
+#baank.py
+#Author: Sarah Hastings
+#read in int, print out, add and return in euro.cent
+
+num1 = int(input("Enter amount1(in cent):"))
+
+
+num2 =  int(input("Enter amount2(in cent):"))
+sum= num1 + num2
+format_sum = format(sum/100)
+
+print (f"The sum of these is €{format_sum}")
+
+
+
