7991156be7abe9cd35acf427fc1f4aa1af26bff1
Sarah Hastings <G00235562@atu.ie>
2023-04-10 16:24:29 +0100

    Topic 05 Tidied op code, use weekday() to get day number, added references used

--- weekday.py
+++ weekday.py
@@ -1,20 +1,17 @@
-#Week 5
+#Weekly task 05
 #Author: Sarah Hastings
 #This program outputs whether or not today is a weekday. 
 #On a weekday it will output "Yes, unfortunately today is a weekday."
 #On the weekend it will output "It is the weekend, yay!"
-#Reference https://www.w3schools.com/python/python_datetime.asp , https://www.programiz.com/python-programming/datetime/current-datetime, 
-#https://www.datacamp.com/tutorial/python-datetime
+#Reference   https://www.w3schools.com/python/python_datetime.asp , 
+            #https://www.programiz.com/python-programming/datetime/current-datetime , 
+            #https://www.datacamp.com/tutorial/python-datetime
+            
+from datetime import datetime       #Firstly need to import "datetime" to work with dates
 
-#from datetime import date #first need to import "datetime" to work with dates as date objects.
-
-#Below is shorter option, tidier code 
-from datetime import datetime
-# Get Day Number from weekday
-weekno = datetime.today().weekday()
+weekno = datetime.today().weekday() # Get Day Number from weekday
  
 if weekno < 5:
     print("Yes, unfortunately today is a weekday.")
 else:  
-    # 5 Sat, 6 Sun
-    print("It is the weekend, yay!")
+    print("It is the weekend, yay!") # 5 Sat, 6 Sun
