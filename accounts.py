4eff9d8566136cb49470ecb6a4b50199b163ac78
Sarah Hastings <G00235562@atu.ie>
2023-04-10 15:13:02 +0100

    Topic 03 Adding step to take an account of any length and assuming the next step is to return account number of any length displaying only last 4 digits and all other digits as X.

--- accounts.py
+++ accounts.py
@@ -1,16 +1,26 @@
-#Week03
+#Weekly task 03
 #Author: Sarah Hastings
 #This program reads in a 10 character account number 
-#Outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs).
-#Reference: https://www.w3schools.com/python/python_strings_slicing.asp ,https://www.freecodecamp.org/news/python-slicing-how-to-slice-an-array/ , https://www.w3schools.com/python/ref_string_rjust.asp
+#Outputs the account number with only the last 4 digits showing and the first 6 digits replaced with Xs.
+#Reference:  https://www.w3schools.com/python/python_strings_slicing.asp , 
+            #https://www.freecodecamp.org/news/python-slicing-how-to-slice-an-array/ , 
+            #https://www.w3schools.com/python/ref_string_rjust.asp
 
-#User is prompted to enter a 10 digit number, there is no restriction on max amount of numbers that can be entered here
-accountno = input("Please enter an 10 digit account number:")
 
-#below will take only input of 10 numbers, keep asking until user enters 10 numbers
-#while len(accountno) != 10:    
-#    accountno = input('That was not a 10 digit number. Please enter your 10 digit number \n')
+#The below will take only input of 10 numbers, keep asking until the user enters 10 numbers, print error message if 10 digits are not entered
+while True:
+    accountno = input("Please enter an 10 digit account number:")
+    if len(accountno) !=10:
+        print('That was not a 10 digit number. Please enter your 10 digit number \n')
+    else:
+        Bank = accountno[-4:].rjust(len(accountno), 'X')                #When 10 digits are entered, the rjust() method will right align the string using X as the fill character to anything before the last 4 digits
+        print(Bank)
+        break
 
-   
-Bank = accountno[-4:].rjust(len(accountno), 'X')
-print(Bank)
+#Extra step to the program:
+#This will modify the program to deal with account numbers of any length 
+#And assuming the next step is to display only the last 4 characters, all other numbers will be replaced with Xs and the end result printed out.
+
+account_no_1 = input("Please enter an account number: ")
+
+print("x" * (len(account_no_1) - 4) + account_no_1[-4:])
